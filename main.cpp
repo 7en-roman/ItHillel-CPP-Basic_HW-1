f56bc668f134be6701bde4eef9ba7b03983b2c7f
Roman Sushko <7en.roman+git@gmail.com>
2025-05-19 18:58:55 -0400

    Initial Version

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// ДЗ 1. Вивести анкету "Про себе" на консоль
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "---- Information ----\n";
+    std::cout << "First name ->\t \'Roman\'\n";
+    std::cout << "Last name ->\t \'Sushko\'\n";
+    std::cout << "Age \\ 20 Years old\n";
+    std::cout << "Course: C++ Basic\n";
+
+    return 0;
+}
